ee16638690f5c5e417863cd56647b48ad429c6be
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-12-16 02:49:46 -0800

    Edits About Section

--- src/pages/About.js
+++ src/pages/About.js
@@ -26,7 +26,7 @@ function About() {
           Ahmad has always been passionate about computers and coding. For over three decades he has been learning, working, troubleshooting and, occasionally, programming, in different languages, on different devices and operating systems, including windows, macOS, iOS, Android, linux, and unix!
         </p>
         <p>
-          He started programming, as a young child, in the mid 1980s, in the programming language of <i>Basic</i>, on <i>Sinclair</i> computers. At the age of 16, Ahmad was admitted to the University of Colorado at Boulder (CU), as a Computer Science major, but switched to Civil Engineering in order to join his family business, in construction. However, he began web‑development, at the rise of the internet, during his time at CU, in the mid 1990s.
+          He started programming, as a young child, in the mid 1980s, in the programming language of <i>Basic</i>, on <i>Sinclair</i> computers. At the age of 16, Ahmad was admitted to the University of Colorado at Boulder (CU), as a Computer Science major, but switched to Civil Engineering in order to join his family business, in construction. However, during his time at CU, he began web‑development, at the rise of the internet, in the mid 1990s.
         </p>
         <p>
           Ahmad's passion for computers and coding continued to grow, so in 2013, he studied mobile‑app development at the University of California Irvine (UCI) extension (online) and produced his first app, which was published to Apple's App Store, in 2014.
@@ -35,7 +35,7 @@ function About() {
           In July 2020, Ahmad upgraded his coding knowledge &amp; skills, advanced his techniques and updated his development tools by joining an online coding bootcamp, for full‑stack web‑development, at the University of California, Berkeley (UCB), extension.
         </p>
         <p>
-          Samples of his work can be seen in the Portfolio section of this website, which demonstrates proficiency in both the front-end and back-end stacks. Please feel free to visit the deployed websites of each project and browse their code repositories on <a href="https://github.com/ahmadelgamal" target="_blank" rel="noreferrer">GitHub</a>.
+          Samples of his work can be seen in the Portfolio section of this website, which demonstrate proficiency in both the front-end and back-end stacks. Please feel free to visit the deployed websites of each project and browse their code repositories on <a href="https://github.com/ahmadelgamal" target="_blank" rel="noreferrer">GitHub</a>.
         </p>
         <p>
           Ahmad was born in Egypt and is also a United States citizen. He lives
