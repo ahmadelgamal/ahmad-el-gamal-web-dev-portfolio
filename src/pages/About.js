0e17a54b462a568193430b3c8309aeaa6ded2394
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-12-16 02:44:01 -0800

    Updates About section

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,13 +23,16 @@ function About() {
           organize and sort almost anything!
         </p>
         <p>
-          Ahmad has always been passionate about computers and coding. He started programming, as a young child, in the 1980s, in the programming language of <i>Basic</i>, on <i>Sinclair</i> computers. He began web‑development, at the rise of the internet, in the 1990s. He also started his univeristy education, at the University of Colorado at Boulder, as a Computer Science major, in 1995, but switched to Civil Engineering in order to join his family business, in construction.
+          Ahmad has always been passionate about computers and coding. For over three decades he has been learning, working, troubleshooting and, occasionally, programming, in different languages, on different devices and operating systems, including windows, macOS, iOS, Android, linux, and unix!
         </p>
         <p>
-          However, his passion for computers and coding continued to grow, so he studied mobile‑app development at the University of California Irvine (UCI) extension (online), in 2013, and produced his first app, which was published to Apple's App Store, in 2014.
+          He started programming, as a young child, in the mid 1980s, in the programming language of <i>Basic</i>, on <i>Sinclair</i> computers. At the age of 16, Ahmad was admitted to the University of Colorado at Boulder (CU), as a Computer Science major, but switched to Civil Engineering in order to join his family business, in construction. However, he began web‑development, at the rise of the internet, during his time at CU, in the mid 1990s.
         </p>
         <p>
-          In July 2020, Ahmad upgraded his coding knowledge, learned more effective skills, advanced his techniques and updated his development tools by joining an online coding bootcamp, for full‑stack web‑development, at the University of California, Berkeley (UCB), extension.
+          Ahmad's passion for computers and coding continued to grow, so in 2013, he studied mobile‑app development at the University of California Irvine (UCI) extension (online) and produced his first app, which was published to Apple's App Store, in 2014.
+        </p>
+        <p>
+          In July 2020, Ahmad upgraded his coding knowledge &amp; skills, advanced his techniques and updated his development tools by joining an online coding bootcamp, for full‑stack web‑development, at the University of California, Berkeley (UCB), extension.
         </p>
         <p>
           Samples of his work can be seen in the Portfolio section of this website, which demonstrates proficiency in both the front-end and back-end stacks. Please feel free to visit the deployed websites of each project and browse their code repositories on <a href="https://github.com/ahmadelgamal" target="_blank" rel="noreferrer">GitHub</a>.
