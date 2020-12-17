bfee6a6fba89b4461e097b7863efbe432c15b488
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-12-16 20:01:19 -0800

    adds private repos to portfolio

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,7 +3,7 @@ import githubLogo from '../assets/images/GitHub-Mark-120px-plus.png'
 
 function Project() {
 
-  const [projects] = useState([
+  const [publicProjects] = useState([
     {
       id: "ucb-my-food-choices",
       name: "My Food Choices",
@@ -102,6 +102,114 @@ function Project() {
       alt: "Ahmad El Gamal's Portfolio",
       link: "https://ahmadelgamal.github.io/",
       repo: "https://github.com/ahmadelgamal/ahmadelgamal.github.io"
+    }, {
+      id: "ucb-git-it-done",
+      name: "Git it Done",
+      category: "templates",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "git-it-done.png",
+      alt: "Git it Done",
+      link: "https://ahmadelgamal.github.io/ucb-git-it-done/",
+      repo: "https://github.com/ahmadelgamal/ucb-git-it-done"
+    },
+    {
+      id: "ucb-robot-gladiators",
+      name: "Robot Gladiators",
+      category: "templates",
+      description: "",
+      tech: "JavaScript",
+      screenshot: "robot-gladiators.png",
+      alt: "Robot Gladiators",
+      link: "https://ahmadelgamal.github.io/ucb-robot-gladiators/",
+      repo: "https://github.com/ahmadelgamal/ucb-robot-gladiators"
+    },
+    {
+      id: "ucb-run-buddy",
+      name: "Run Buddy",
+      category: "templates",
+      description: "",
+      tech: "HTML5 | CSS3",
+      screenshot: "run-buddy.png",
+      alt: "Run Buddy",
+      link: "https://ahmadelgamal.github.io/ucb-run-buddy/",
+      repo: "https://github.com/ahmadelgamal/ucb-run-buddy"
+    },
+    {
+      id: "ucb-taskinator",
+      name: "Taskinator",
+      category: "templates",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "taskinator.png",
+      alt: "Taskinator",
+      link: "https://ahmadelgamal.github.io/ucb-taskinator/",
+      repo: "https://github.com/ahmadelgamal/ucb-taskinator"
+    },
+    {
+      id: "ucb-taskmaster-pro",
+      name: "Taskmaster Pro",
+      category: "templates",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "taskmaster-pro.png",
+      alt: "Taskmaster Pro",
+      link: "https://ahmadelgamal.github.io/ucb-taskmaster-pro/",
+      repo: "https://github.com/ahmadelgamal/ucb-taskmaster-pro"
+    },
+  ]);
+
+  const [privateProjects] = useState([
+    {
+      id: "lifetime-egypt",
+      name: "Lifetime Egypt",
+      category: "featured",
+      description: "App displays a catalog of products, with specifications, pictures and prices, for a furniture company.",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "lifetime-egypt.png",
+      alt: "Lifetime Egypt",
+      link: "https://lifetimeegypt.com/",
+      repo: "https://github.com/ahmadelgamal/lifetime-egypt"
+    },
+    {
+      id: "crescent-trade",
+      name: "Crescent Trade",
+      category: "private",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "crescent-trade.png",
+      alt: "Crescent Trade",
+      link: "https://crescenttrade.com/",
+    },
+    {
+      id: "elgamal-group",
+      name: "El Gamal Group",
+      category: "private",
+      description: "",
+      tech: "iWeb",
+      screenshot: "elgamal-group.png",
+      alt: "El Gamal Group",
+      link: "http://elgamalgroup.com/",
+    },
+    {
+      id: "al-azhar",
+      name: "El Gamal's Azhari Guide",
+      category: "private",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "alazhar.png",
+      alt: "El Gamal's Azhari Guide",
+      link: "https://ahmadelgamal.com/alazhar/",
+    },
+    {
+      id: "ahmadelgamal",
+      name: "Ahmad's Personal Page",
+      category: "private",
+      description: "",
+      tech: "HTML5 | CSS3 | JavaScript",
+      screenshot: "imam-ahmad.png",
+      alt: "Imam Ahmad El Gamal",
+      link: "https://ahmadelgamal.com/",
     },
   ]);
 
@@ -118,43 +226,76 @@ function Project() {
 
       <h1>Coding Portfolio</h1>
       <div id="projects" className="container">
-        { projects.map((project) => (
+        <div className="public-repo">
+          <h2>PUBLIC REPO PROJECTS</h2>
+          { publicProjects.map((project) => (
 
-          <article key={ project.id } id={ project.id } className={ project.category }>
-            <h3>
-              { project.name }
-            </h3>
+            <article key={ project.id } id={ project.id } className={ project.category }>
+              <h3>
+                { project.name }
+              </h3>
 
-            <a
-              href={ project.link }
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={ require(`../assets/images/screenshots/${project.screenshot}`).default }
-                alt={ project.alt }
-              />
-            </a>
-            <a
-              href={ project.repo }
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                className="github-logo"
-                src={ githubLogo }
-                alt="GitHub Repo link"
-              />
-            </a>
-            <h5>
-              { project.tech }
-            </h5>
-            <p>
-              { project.description }
-            </p>
-          </article>
+              <a
+                href={ project.link }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={ require(`../assets/images/screenshots/${project.screenshot}`).default }
+                  alt={ project.alt }
+                />
+              </a>
+              <a
+                href={ project.repo }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="github-logo"
+                  src={ githubLogo }
+                  alt="GitHub Repo link"
+                />
+              </a>
+              <h5>
+                { project.tech }
+              </h5>
+              <p>
+                { project.description }
+              </p>
+            </article>
+          )) }
+        </div>
 
-        )) }
+        <hr />
+
+        <div className="private-repo">
+          <h2>PRIVATE REPO PROJECTS</h2>
+          { privateProjects.map((project) => (
+
+            <article key={ project.id } id={ project.id } className={ project.category }>
+              <h3>
+                { project.name }
+              </h3>
+
+              <a
+                href={ project.link }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={ require(`../assets/images/screenshots/${project.screenshot}`).default }
+                  alt={ project.alt }
+                />
+              </a>
+              <h5>
+                { project.tech }
+              </h5>
+              <p>
+                { project.description }
+              </p>
+            </article>
+          )) }
+        </div>
       </div>
     </>
   );
