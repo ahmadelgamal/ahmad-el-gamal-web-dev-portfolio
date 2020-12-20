b2f48125e7715cfec27b3d7a43ff7e760d89ce96
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-12-20 01:44:01 -0800

    Adds send email from contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { validateEmail } from '../utils/helpers';
+import emailjs from 'emailjs-com';
 
 function Contact() {
   useEffect(() => {
@@ -31,13 +32,26 @@ function Contact() {
     if (!errorMessage) {
       setFormState({ ...formState, [event.target.name]: event.target.value })
     }
-
-    // console.log('errorMessage: ', errorMessage);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formState);
+    // console.log(formState);
+    emailjs.sendForm('service_xbl8m76', 'template_yy13ndc', event.target, 'user_3QlmspmFC6ohkE8C94IEp')
+      .then((result) => {
+        // console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
+      .then(() => {
+        setErrorMessage('Message sent successfully!');
+        setTimeout(function () { setErrorMessage(''); }, 3000);
+      })
+      .then(() => {
+        event.target.querySelector("input[name='name'").value = "";
+        event.target.querySelector("input[name='email'").value = "";
+        event.target.querySelector("textarea[name='message'").value = "";
+      });
   }
 
   return (
@@ -50,7 +64,7 @@ function Contact() {
       </section>
 
       <section id="contacts" className="container">
-      <h1>Contact Ahmad El Gamal</h1>
+        <h1>Contact Ahmad El Gamal</h1>
         <form id="contact-form" onSubmit={ handleSubmit }>
           <div>
             <label htmlFor="name">Name:</label>
@@ -64,12 +78,12 @@ function Contact() {
             <label htmlFor="message">Message:</label>
             <textarea id="message-input" type="text" name="message" rows="7" placeholder="Please type your message here" defaultValue={ message } onBlur={ handleChange } />
           </div>
+          <button id="contact-submit" type="submit" value="send">Submit</button>
           { errorMessage && (
             <div>
               <p id="contact-form-error-message">{ errorMessage }</p>
             </div>
           ) }
-          <button id="contact-submit" type="submit" >Submit</button>
         </form>
       </section>
     </>
