2a726fa74f5126a55db2cab89a8e4e60c48aa16a
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-03-02 14:08:29 -0800

    Updates Resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -30,8 +30,59 @@ function Resume() {
           </a>
         </h2>
 
-        {/* <object width="100%" height="1080px" data={resume} type="application/pdf">   </object> */}
+        <h3>Work Experience</h3>
 
+        <div className='flex mt-20'>
+          <h4>President</h4>
+          <h4>June 2015 - Present</h4>
+        </div>
+        <div className='flex'>
+          <h4>Crescent Academies LLC</h4>
+          <h4>Irvine, CA</h4>
+        </div>
+        <ul>
+          <li>Intriguingly taught front-end web-development to elementary and middle-school students.</li>
+          <li>Creatively taught digital photography to elementary and middle-school students.</li>
+        </ul>
+
+        <div className='flex mt-20'>
+          <h4>President</h4>
+          <h4>November 2011 - Present</h4>
+        </div>
+        <div className='flex'>
+          <h4>Crescent Holdings LLC</h4>
+          <h4>Irvine, CA</h4>
+        </div>
+        <ul>
+          <li>Patiently produced a mobile app (iOS and Android), that was downloaded all around the world.</li>
+          <li>Aggressively launched an online furniture store with a facebook page that exceeded 50k likes in year 1.</li>
+          <li>Created an online women’s fashion store that had two physical storefronts in Orange County, CA.</li>
+        </ul>
+
+        <h3 className='mt-40'>Education</h3>
+
+        <div className='flex mt-20'>
+          <h4>Certified Full-Stack Web-Developer</h4>
+          <h4>Jan 2021</h4>
+        </div>
+        <div className='flex'>
+          <h4>University of California, Extension</h4>
+          <h4>Berkeley, CA</h4>
+        </div>
+        <ul>
+          <li>Online Full-Stack Web-Development Certificate</li>
+        </ul>
+
+        <div className='flex mt-20'>
+          <h4>Civil Engineer</h4>
+        </div>
+        <div className='flex'>
+          <h4>University of Colorado</h4>
+          <h4>Boulder, CO</h4>
+        </div>
+        <ul>
+          <li>Bachelor of Science in Civil Engineering</li>
+        </ul>
       </section>
     </>
   );
